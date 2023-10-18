824e00be8f9b1a883cd10aaab2e7e7d16e96b112
santty22 <144271939+santty22@users.noreply.github.com>
2023-10-18 14:54:39 -0300

    Add files via upload

---
+++ Algoritmos2023-main/ejercicio 1 practica parcial/ejercicio 1 practica parcial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ejercicio_1_practica_parcial
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ArrayList lista= new ArrayList();
+			string respuesta;
+			
+			Console.WriteLine("Ingresa números? (s/n)");
+			respuesta=Console.ReadLine();
+			while (respuesta=="s")
+			{
+				Console.WriteLine("Ingrese un número para agregar a la lista:");
+				int numero = int.Parse(Console.ReadLine());
+				lista.Add(numero);
+				Console.WriteLine("Sigue ingresando números? (s/n)");
+				respuesta=Console.ReadLine();
+			}
+			foreach(int num in lista)
+			{
+				if (num<0)
+				{
+					Console.WriteLine("La lista posee numeros negativos.");
+					break;
+				}
+			}
+			
+			Console.ReadKey(true);
+		}
+	}
+}
